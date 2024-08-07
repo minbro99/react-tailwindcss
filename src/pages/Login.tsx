60728f46d54b03547a1c9b5b0baa80d22daad01f
minbro99 <138020121+minbro99@users.noreply.github.com>
2024-08-07 18:32:33 +0900

    Feat: 추가 특정 아이디 비밀번호 입력 오류시 오류 메시지
    특정한 형식의 아이디와 비밀번호 양식대로 입력하지 않으면 
    아이디와 비밀번호에 대한 입력방식의 오류 메시지 출력하는 if-else 함수문 추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,16 +3,29 @@ import React, { useState } from "react";
 export const Login = () => {
     const [userName, setUserName] = useState('');
     const [userPassword, setUserPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
-    const handleLoginClick = (event) => {
+    const handleLoginClick = (event: { preventDefault: () => void; }) => {
         event.preventDefault();
-    
+
         const validUserName = 'admin';
         const validPassword = 'nuri1004!';
-        
+        const englishRegex = /^[A-Za-z]+$/;
+        const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+
+        if (!englishRegex.test(userName)) {
+            setErrorMessage('아이디 형식이 잘못되었습니다.');
+            return;
+        } else if (!passwordRegex.test(userPassword)) {
+            setErrorMessage('비밀번호 형식이 잘못되었습니다. 최소 8자, 영어, 숫자 및 특수문자가 포함되어야 합니다.');
+            return;
+        } else {
+            setErrorMessage('');
+        }
+
         if (userName === validUserName && userPassword === validPassword) {
             window.location.href = '/';
-        } else {            
+        } else {
             alert('잘못된 사용자 이름 또는 비밀번호입니다.');
         }
     };
@@ -22,6 +35,11 @@ export const Login = () => {
             <div className="bg-white w-full max-w-lg py-10 rounded-lg text-center">
                 <h3 className="text-3xl text-gray-800">로그인</h3>
                 <form className="flex flex-col mt-5 px-5" onSubmit={handleLoginClick}>
+                <div className="text-sm">
+                        <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                            ID 찾기
+                        </a>
+                    </div>
                     <input
                         type="text"
                         placeholder="Email"
@@ -31,7 +49,7 @@ export const Login = () => {
                     />
                     <div className="text-sm">
                         <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                            ID 찾기
+                            비밀번호 찾기
                         </a>
                     </div>
                     <input
@@ -41,11 +59,11 @@ export const Login = () => {
                         value={userPassword}
                         onChange={(e) => setUserPassword(e.target.value)}
                     />
-                    <div className="text-sm">
-                        <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                            비밀번호 찾기
-                        </a>
-                    </div>
+                    {errorMessage && (
+                        <div className="text-red-500 text-sm mt-1">{errorMessage}</div>
+                    )}
+                    
+                    
                     <button
                         type="submit"
                         className="py-3 px-5 bg-gray-600 text-white mt-3 text-lg rounded-lg focus:outline-none hover:opacity-90"
