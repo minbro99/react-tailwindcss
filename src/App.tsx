a6be95cf9161f8358edb84ef3ca939e23d4f8e82
minbro99 <138020121+minbro99@users.noreply.github.com>
2024-08-07 15:11:10 +0900

    Feat: HomeLogin에 대한 라우터설정,, Rename: Login파일 이름변경
     HomeLogin에 대한 라우터를 새로 추가하여 메인 페이지에 로그인 창을 띄우게 하고, Login파일 이름을 homelogin으로 파일 이름 변경하여 라우터까지 설정

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,34 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Header from './components/Header';
 import Supporter from './components/Supporter';
+import {HomeLogin} from './components/HomeLogin';
 
 import LearnMore from "./pages/LearnMore";
-import Login from './pages/Login';
+
 import MyProfile from './pages/MyProfile';
 import Setting from './pages/MyProfile';
 import ShoppingCart from "./pages/Shoppingcart";
+import Home from './pages/Home';
+import IdFind from "./pages/IdFind";
+
 
 const App: React.FC = () => {
   return(
     <>
+    <HomeLogin />
     <Router>
+    
     {/* <Main />      */}
     <NavBar />
     <Header />
     <Routes>
+      
       <Route path="/learnmore" element={<LearnMore />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/myprofile" element={<MyProfile />} />
       <Route path="/setting" element={<Setting />} />
       <Route path="/shoppingcart" element={<ShoppingCart />} />
+      <Route path="/idfid" element={<IdFind />} />
     </Routes>
     <Supporter />
     </Router>
