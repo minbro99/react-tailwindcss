8922ee478b4036591d9c562c5038917de4353c71
minbro99 <138020121+minbro99@users.noreply.github.com>
2024-08-07 18:42:16 +0900

    Feat: loginsuccess의 라우터 링크 추가
    loginsucces에 대한 라우터 링크 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Main from './components/Main';
+import Header from './components/Header';
 
 import Home from './pages/Home';
 import { Login } from "./pages/Login";
+import LearnMore from './pages/LearnMore';
+import LoginSuccess from './pages/LoginSuccess'
 
 const App: React.FC = () => {
   return(
@@ -13,9 +16,12 @@ const App: React.FC = () => {
     
     <Router>
     <Routes>
+      <Route path="/header" element={<Header/>} />
       <Route path="/" element={<Main />} />
       <Route path="/home" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/learnmore" element={<LearnMore/>} />
+      <Route path="/loginsuccess" element={<LoginSuccess />} />
     </Routes>
     </Router>
     </>
