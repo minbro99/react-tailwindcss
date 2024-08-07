657c28edff532b5562b9c5ea2032c2a681f5a363
minbro99 <138020121+minbro99@users.noreply.github.com>
2024-08-07 18:22:39 +0900

    Feat: 추가 특정 아이디 비밀번호 입력하면 페이지 이동
    admin, nuri1004!를 입력해야만 '/' 링크로 이동하게 설정, 만약 아이디 비밀번호가 다를시 alert창을 띄워 아이디 비밀번호 잘못 입력한 정보를 알림

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,100 +1,59 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
-export default function Example() {
+import React, { useState } from "react";
+
+export const Login = () => {
+    const [userName, setUserName] = useState('');
+    const [userPassword, setUserPassword] = useState('');
+
+    const handleLoginClick = (event) => {
+        event.preventDefault();
+    
+        const validUserName = 'admin';
+        const validPassword = 'nuri1004!';
+        
+        if (userName === validUserName && userPassword === validPassword) {
+            window.location.href = '/';
+        } else {            
+            alert('잘못된 사용자 이름 또는 비밀번호입니다.');
+        }
+    };
+
     return (
-      <>
-        {/*
-          This example requires updating your template:
-  
-          ```
-          <html class="h-full bg-white">
-          <body class="h-full">
-          ```
-        */}
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-              alt="Your Company"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              className="mx-auto h-10 w-auto"
-            />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Sign in to your account
-            </h2>
-          </div>
-  
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form action="#" method="POST" className="space-y-6">
-              <div>
-                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                  Email address
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    required
-                    autoComplete="email"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-  
-              <div>
-                <div className="flex items-center justify-between">
-                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                    Password
-                  </label>
-                  <div className="text-sm">
-                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                      Forgot password?
-                    </a>
-                  </div>
-                </div>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    required
-                    autoComplete="current-password"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-  
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Sign in
-                </button>
-              </div>
-            </form>
-  
-            <p className="mt-10 text-center text-sm text-gray-500">
-              Not a member?{' '}
-              <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                Start a 14 day free trial
-              </a>
-            </p>
-          </div>
+        <div className="h-screen flex items-center justify-center bg-gray-800">
+            <div className="bg-white w-full max-w-lg py-10 rounded-lg text-center">
+                <h3 className="text-3xl text-gray-800">로그인</h3>
+                <form className="flex flex-col mt-5 px-5" onSubmit={handleLoginClick}>
+                    <input
+                        type="text"
+                        placeholder="Email"
+                        className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg"
+                        value={userName}
+                        onChange={(e) => setUserName(e.target.value)}
+                    />
+                    <div className="text-sm">
+                        <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                            ID 찾기
+                        </a>
+                    </div>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 py-3 px-5 rounded-lg"
+                        value={userPassword}
+                        onChange={(e) => setUserPassword(e.target.value)}
+                    />
+                    <div className="text-sm">
+                        <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                            비밀번호 찾기
+                        </a>
+                    </div>
+                    <button
+                        type="submit"
+                        className="py-3 px-5 bg-gray-600 text-white mt-3 text-lg rounded-lg focus:outline-none hover:opacity-90"
+                    >
+                        로그인
+                    </button>
+                </form>
+            </div>
         </div>
-      </>
-    )
-  }
-  
+    );
+};
